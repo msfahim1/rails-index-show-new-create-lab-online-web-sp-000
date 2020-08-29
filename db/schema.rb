409a1f13ff762ce95661d5fbd7b26711653edf1d
Michael Fahim <michael_fahim@yahoo.com>
2020-08-29 18:21:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200523015505) do
-
-  create_table "coupons", force: :cascade do |t|
-    t.string   "coupon_code"
-    t.string   "store"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
